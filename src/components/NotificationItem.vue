b3bc97a31e95256214ae5d5933eea6bcd68e32f9
Tomas F. Carrion <tomas.carrion@academyex.com>
2023-09-22 00:31:56 +1200

    feat: add on click event

--- src/components/NotificationItem.vue
+++ src/components/NotificationItem.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
+
 export interface Notification {
   id: string
   title: string
@@ -13,10 +16,53 @@ export interface Notification {
 const { notification } = defineProps<{
   notification: Notification
 }>()
+
+const notificationError = ref<string | null>(null)
+const notificationSuccess = ref<string | null>(null)
+
+
+const handleNotificationClick = async () => {
+  console.log(notification.available_actions)
+  try {
+    if (notification.available_actions.includes('CREATE_TODO')) {
+      // Action is available, make the "CREATE TODO" API call
+      const response = await axios.post('https://o59ee.wiremockapi.cloud/notifications/action', {
+        id: notification.id,
+        action: 'TODO'
+      })
+
+      notificationSuccess.value = 'TODO added'
+      setTimeout(() => {
+        notificationSuccess.value = null
+    }, 2000)
+
+
+    } else {
+      // "CREATE TODO" action is not available, make the "ignore" API call
+      const response = await axios.post('https://o59ee.wiremockapi.cloud/notifications/ignore', {
+        id: notification.id
+      })
+
+      notificationSuccess.value = 'Notification ignored'
+      setTimeout(() => {
+        notificationSuccess.value = null
+    }, 2000)
+
+    }
+  } catch (error) {
+    // Handle any errors (e.g., display an error message)
+    console.error('Error:', error)
+    notificationError.value = "Can't add TODO action to this notification"
+    // Clear the error message after 1 second
+    setTimeout(() => {
+      notificationError.value = null
+    }, 2000)
+  }
+}
 </script>
 
 <template>
-  <div class="flex-row wrapper">
+  <div class="flex-row wrapper" @click="handleNotificationClick">
     <div v-if="!notification.read" class="dot"></div>
     <div class="flex-col info">
       <h1 class="title">{{ notification.title }}</h1>
@@ -25,6 +71,16 @@ const { notification } = defineProps<{
         <h2 class="author">{{ notification.author }}</h2>
         <h2 class="author">{{ notification.created }}</h2>
       </span>
+      <transition name="fade" mode="out-in">
+      <div v-if="notificationError" class="error">
+        {{ notificationError }}
+      </div>
+    </transition>
+    <transition name="fade" mode="out-in">
+      <div v-if="notificationSuccess" class="success">
+        {{ notificationSuccess }}
+      </div>
+    </transition>
     </div>
   </div>
 </template>
@@ -69,5 +125,30 @@ const { notification } = defineProps<{
 }
 .author {
   color: #ffffff99;
+}
+
+.error {
+  background-color: #DF6873;
+  color: white;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  font-weight: bold;
+  text-align: center;
+}
+.success {
+  background-color: #80DED9;
+  color: white;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  font-weight: bold;
+  text-align: center;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease-in-out;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
